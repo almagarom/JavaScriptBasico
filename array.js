cf39ce55f10d97b59bb501a37e9d6f1aa3cf1d1e
almagarom <87401091+almagarom@users.noreply.github.com>
2023-04-05 13:30:47 -0600

    Add complete array file

--- array.js
+++ array.js
@@ -1,4 +1,39 @@
 //generamos una variable en donde se guardarán varios datod
-var array1 = ["guayaba", "fresa", "piña"]
+var frutas = ["guayaba", "fresa", "piña"]
 //si lleva más de un elemento, van separados por comas
+
+//para consultar la longitud del arreglo
+var longitud = frutas.length
+console.log()
+
+
+//Métodos
+
+//PUSH
+//para agregar un elemento al final
+frutas.push("mango")
+console.log(frutas)
+
+//POP
+//elimina el último elemento del array
+//no es necesario escribir nada entre el parentesis
+frutas.pop()
+console.log(frutas)
+//devolverá el array sin el mango
+
+
+//UNSHIFT
+frutas.unshift("sandía")
+console.log(frutas)
+//agregará el nuevo elemento al inicio
+
+//SHIFT
+frutas.shift()
+//elimina el elemento que se encuentra en la primera posicion
+console.log(frutas)
+
+//INDEX OF
+var posFresa = frutas.indexOf("fresa")
+console.log(posFresa)
+
 
